88160086d0564eb0fefcd920cecbdd6811a3628e
nathalie <harynathago@gmail.com>
2016-07-20 11:56:57 +0300

    mise à jour document et script install.bat

---
+++ cmd/install_authproxy.bat
@@ -0,0 +1,5 @@
+C:
+cd C:\wamp\www\
+rem proxy authentifié (proxyuser, proxypwd, url_proxy et port à remplacer)
+git config --global http.proxy http://proxyuser:proxypwd@url_proxy:port 
+git clone https://github.com/evaluationen/eval_primaire.git/
